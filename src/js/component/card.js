a2c7a8b6bf47a54cb3eda88c820a3e2962b93aa3
Walt Castillo <walt.castillo@gmail.com>
2021-03-12 18:35:20 +0000

    tomen zadicos

---
+++ src/js/component/card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const Card = props => {
+	return (
+		<div className="card" style={{ width: "18rem;" }}>
+			<img className="card-img-top" src={props.image} alt="Card image cap" />
+			<div className="card-body">
+				<h5 className="card-title">{props.title}</h5>
+				<p className="card-text">
+					Some quick example text to build on the card title and make up the bulk of the cards content.
+				</p>
+				<a href="#" className="btn btn-primary">
+					Go somewhere
+				</a>
+			</div>
+		</div>
+	);
+};
+
+Card.propTypes = {
+	image: PropTypes.string,
+	title: PropTypes.string
+};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,20 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+//components
+import { Card } from "../component/card";
+
+export const Home = () => {
+	return (
+		<div className="text-center mt-5">
+			<div>
+				<Card
+					title={"Luke"}
+					image={
+						"https://suttonsquareraleigh.com/wp-content/uploads/sites/2/2018/02/placeholder-400x200-300x150.png"
+					}
+				/>
+			</div>
+		</div>
+	);
+};
