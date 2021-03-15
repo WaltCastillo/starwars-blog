8d19f859b12c76bb2d2e0d3f2fa6ea418d89a30c
Walt Castillo <walt.castillo@gmail.com>
2021-03-15 21:07:02 +0000

    cards layout

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 export const Card = props => {
 	return (
-		<div className="card" style={{ width: "18rem;" }}>
+		<div className="card" style={{ width: "250px", heigt: "400px" }}>
 			<img className="card-img-top" src={props.image} alt="Card image cap" />
 			<div className="card-body">
 				<h5 className="card-title">{props.title}</h5>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -6,14 +6,50 @@ import { Card } from "../component/card";
 
 export const Home = () => {
 	return (
-		<div className="text-center mt-5">
-			<div>
-				<Card
-					title={"Luke"}
-					image={
-						"https://suttonsquareraleigh.com/wp-content/uploads/sites/2/2018/02/placeholder-400x200-300x150.png"
-					}
-				/>
+		<div className="container">
+			<div classNameName="text-center mt-5">
+				<div className="scrolling-wrapper">
+					<div className="card">
+						<Card
+							title={"Luke"}
+							image={
+								"https://suttonsquareraleigh.com/wp-content/uploads/sites/2/2018/02/placeholder-400x200-300x150.png"
+							}
+						/>
+					</div>
+					<div className="card">
+						<Card
+							title={"Luke"}
+							image={
+								"https://suttonsquareraleigh.com/wp-content/uploads/sites/2/2018/02/placeholder-400x200-300x150.png"
+							}
+						/>
+					</div>
+					<div className="card">
+						<Card
+							title={"Luke"}
+							image={
+								"https://suttonsquareraleigh.com/wp-content/uploads/sites/2/2018/02/placeholder-400x200-300x150.png"
+							}
+						/>
+					</div>
+					<div className="card">
+						<Card
+							title={"Luke"}
+							image={
+								"https://suttonsquareraleigh.com/wp-content/uploads/sites/2/2018/02/placeholder-400x200-300x150.png"
+							}
+						/>
+					</div>
+					<div className="card">
+						<Card
+							title={"Luke"}
+							image={
+								"https://suttonsquareraleigh.com/wp-content/uploads/sites/2/2018/02/placeholder-400x200-300x150.png"
+							}
+						/>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
