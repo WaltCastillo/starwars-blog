f5b2b920b565ce777584ec813a75bfa4ef7f59a7
Walt Castillo <walt.castillo@gmail.com>
2021-03-16 16:12:16 +0000

    carousel

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,8 @@ export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<a href="https://i.ytimg.com/vi/Jr06bw2l09M/maxresdefault.jpg">Rusty Pirates </a>
+			<i className="fa fa-bone text-danger" />
 		</p>
 	</footer>
 );
